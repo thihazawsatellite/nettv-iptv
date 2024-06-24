bbc5d71163c1b487612d4d39cc083fc1a95bf02b
thihazawsatellite <88990819+thihazawsatellite@users.noreply.github.com>
2024-06-25 02:46:49 +0630

    Update Play.js
    Signed-off-by: thihazawsatellite <88990819+thihazawsatellite@users.noreply.github.com>

--- src/Play.js
+++ src/Play.js
@@ -183,7 +183,7 @@ export default function Play() {
   };
 
   return (
-    <Page title="Ellipto IPTV">
+    <Page title="NetTV IPTV Player">
       <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}>
         <Box sx={{ width: "100%" }}>
           <Box sx={{ flex: 1, height: "auto", position: "sticky", top: 54 }}>
